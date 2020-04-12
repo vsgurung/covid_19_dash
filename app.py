38cacf053c0a17585ebf33f4f74eae38eec27c99
vsgurung <18561284+vsgurung@users.noreply.github.com>
2020-04-12 11:24:50 +0000

    Add files via upload
    Updated the layout, so that the dashboard is suitable for mobile devices too.

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from utils.check_net_connectivity import connection_status
 from utils import fetch_data, prep_data, prep_graph_figures
 from utils.prep_data import total_england_cases, total_scotland_cases, \
                             total_ni_cases, total_wales_cases
-from utils.prep_html_tags import    generate_heading_tag, generate_markdown_tag, generate_left_column_tags, \
-                                    generate_right_column_tags, generate_footer_tags
+from utils.prep_html_tags import    generate_heading_tag, generate_markdown_tag, generate_left_column_figures, \
+                                    generate_right_column_figures, generate_footer_tags, generate_number_plates
 
 external_stylesheets = ['https://fonts.googleapis.com/css?family=Nunito', dbc.themes.BOOTSTRAP] 
 
@@ -26,22 +26,23 @@ app.title = 'COVID-19 UK Metrics'
 
 # Application layout v1.
 suppress_callback_exceptions=True
-app.layout = html.Div([
-            html.Div(id='heading-tag'),
-            html.Div(id='markdown-tag', className='container-fluid', style={'backgroundColor': '#ffffff', 'border': '1px solid #ededee'}),
-            html.Div([
-                # Left column with UK metrics and County/Unitary Authority Cases
-                html.Div(id='left-column-tag', style={'width':'40%','backgroundColor':'#B9BBBD'}),
-                # Right Column with metrics and the graphs
-                html.Div(id='right-column-tag', style={'width':'60%','backgroundColor':'#B9BBBD'}),
-                ], className="container-fluid", style={'display':'flex'}),
-                html.Div(id='footer-tag'),
+
+app.layout = html.Div(
+            [
+                html.Div(id='heading-tag'),
+                html.Div(id='markdown-tag'),
+                html.Div(id='number-plate'),
+                dbc.Row([
+                    html.Div(id='left-column-figure'),
+                    html.Div(id='right-column-figure')],
+                    align='center', justify='center', no_gutters=True),
+                html.Div(generate_footer_tags()),
                 dcc.Interval(
                     id='timer',
                     interval = 1*900000, # the page should refresh every 15 minutes
                     n_intervals=0
                 )
-        ], className='container-fluid',  style={'fontFamily':'nunito', 'backgroundColor':'#ffffe6'})
+            ])
 
 
 # All the callbacks 
@@ -59,26 +60,27 @@ def update_markdown(n):
     md = generate_markdown_tag()
     return md
 
-@app.callback(Output('left-column-tag', 'children'),
+@app.callback(Output('number-plate', 'children'),
 [Input('timer', 'n_intervals')])
 
-def update_left_column_tag(n):
-    lt = generate_left_column_tags()
+def update_number_plate(n):
+    num_plates = generate_number_plates()
+    return num_plates
+
+@app.callback(Output('left-column-figure', 'children'),
+[Input('timer', 'n_intervals')])
+
+def update_left_column_figures(n):
+    lt = generate_left_column_figures()
     return lt
 
-@app.callback(Output('right-column-tag', 'children'),
+@app.callback(Output('right-column-figure', 'children'),
 [Input('timer', 'n_intervals')])
 
-def update_right_column_tag(n):
-    rt = generate_right_column_tags()
+def update_right_column_figures(n):
+    rt = generate_right_column_figures()
     return rt
 
-@app.callback(Output('footer-tag', 'children'),
-[Input('timer', 'n_intervals')])
-
-def update_footer_tag(n):
-    rt = generate_footer_tags()
-    return rt
 
 
 # Running the app
